e3edbd4d47c15f46f45fcf3cdfaa3a1be770c21c
sourav barman <63946634+souravb257@users.noreply.github.com>
2021-08-28 11:46:56 +0600

    add,sub etc, floor, power using

---
+++ Numerical/Numerical.py
@@ -0,0 +1,22 @@
+'''
+print(10+5)
+print(10-5)
+print(10*5)
+print(10/5)
+print(10%5)
+'''
+#Using varibale and storing data
+a= 20
+b= 5
+c = 38
+d = 5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+
+#Floor Purno sonkha
+print(c//d)
+#Power using
+print(2**4)
