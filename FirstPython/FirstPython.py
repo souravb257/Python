8ae87dca7ee999269dc4e22ae79af94995602e26
sourav barman <63946634+souravb257@users.noreply.github.com>
2021-08-28 11:07:25 +0600

    FirstPython Code

---
+++ FirstPython/FirstPython.py
@@ -0,0 +1,5 @@
+print("Sourav Barman")
+print(10)
+print("Sourav Barman \n Sourav Barman")#print in one line using n
+print("Sourav Barman \t Sourav Barman") #for space
+print("\"Sourav Barman \t Sourav Barman\"") #for show double cotation
