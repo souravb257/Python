2f218be9a0c29b86db5a12ca1d80af5f9b71ad14
sourav barman <63946634+souravb257@users.noreply.github.com>
2021-08-28 11:27:21 +0600

    Name, age change using Variable without coding

---
+++ Variable/Variable.py
@@ -0,0 +1,9 @@
+name = "sourav barman"
+age = 23
+cgpa = 3.95
+print("sourav" + " Barman" + " Dhaka") #For connect string value
+print("My name is " +name)
+print(name+ " lives in dhaka")
+print(name+ " is currently" , age , "year old") #for int value concate should be use ,
+print("At the age of " , age , " he started to learn python")
+print(name+ " achived",cgpa ,"in this previous semister ")
