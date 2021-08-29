ad4739976f5c651ce18407fdcc073c49c06e1c02
sourav barman <63946634+souravb257@users.noreply.github.com>
2021-08-29 13:47:30 +0600

    Trapezium

---
+++ AreaOfAnyShape/Trapezium.py
@@ -0,0 +1,6 @@
+upperBase = float(input("Enter Upper Base:"))
+lowerBase = float(input("Enter Lower Base:"))
+verticalHeight = float(input("Enter Verical Height:"))
+
+area = 0.5*(upperBase + lowerBase) * verticalHeight #Trapezium ecuation
+print(area)
