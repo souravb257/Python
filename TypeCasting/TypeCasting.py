eb6da0c79160adf12196514bf05142f35dc15e94
sourav barman <63946634+souravb257@users.noreply.github.com>
2021-08-29 13:15:44 +0600

    simple TypeCasting

---
+++ TypeCasting/TypeCasting.py
@@ -0,0 +1,14 @@
+
+#Typecasting basically use for converting the data type suppose string to int.
+
+num1 = input("Enter your first number:")#string
+num2 = input("Enter your secound number:")#string
+
+result = int(num1) + int(num2) #Converting string to int
+print("The sum is:" ,result)
+result = int(num1) - int(num2) #Converting string to int
+print("The sub is:" ,result)
+result = int(num1) * int(num2) #Converting string to int
+print("The mul is:" ,result)
+result = int(num1) / int(num2) #Converting string to int
+print("The div is:" ,result)
